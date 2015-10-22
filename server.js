a611fc16cb1a1bf0870cbe6b4c5e6042c7e97421
Tyler Becker <tyler.becker@selu.edu>
2015-10-21 21:59:27 -0500

    Fixed code formatting and found how to extract the byte array that the binaryjs forms after a .wav file is submitted through it. This is in a separate branch because the byte array is transferred over web sockets rather than being grabbed from the request body on a post request. The main goal is to take that byte array and pipe it straight into pocket sphynx.

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-//server.js homie
-
-
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -10,90 +6,55 @@ var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
 var http = require('http');
 
-
-
-//TODO: make front end that will transfer audio to server 
-//TODO: will transfer audio in an array of bytes
-
-// this will let us get the data from a POST
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.json());
-
+// This will let us get the data from a POST
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-//var bs = new BinaryServer({port: 3000});
-//var port = process.env.PORT || 9000;   
-
+   
 var port = process.env.PORT || 80;
 
-// routes for api
+// Routes for api
 var router = express.Router();
 
+router.get('/', function (req, res) {
+    console.log("We made it");
+    res.sendfile('public/index.html');
+});
 
+router.post('/audio', function (req, res) {
+    console.log("Your audio is being translated");
+});
 
-
-    router.get('/', function (req, res) {
-        console.log("We made it");
-        res.sendfile('public/index.html');
-    });
-
-
-
-
-// more routes for our API will happen here
-
-
-
-    router.post('/audio', function (req, res) {
-
-        console.log("Your audio is being translated");
-
-
-
-
-
-
-    });
-
-
-//CHANGES: jkdsajkhfdsajkl
-
-    // REGISTER OUR ROUTES -------------------------------
-    // all of our routes will be prefixed with /api
-    app.use('/api', router);
-
+// REGISTER OUR ROUTES
+app.use('/api', router);
 
 var server = http.createServer(app);
 
 // Start Binary.js server
-//var BinaryServer = require('../../').BinaryServer;
-//var bs = BinaryServer({server: server});
 var bs = new BinaryServer({port: 3000});
 
 // Wait for new user connections
 bs.on('connection', function(client){
   // Incoming stream from browsers
   client.on('stream', function(stream, meta){
-    //old
-    //var file = fs.createWriteStream(__dirname+ '/public/' + meta.name);
 	var file = fs.createWriteStream(__dirname+ '/node_modules/voice/wav/wavin.wav');    
 	stream.pipe(file);
-    //
-    // Send progress back
     stream.on('data', function(data){
+      //Data parameter is the byte array representation of the .wav file.
+      //Here will be a method call to send the byte array to pocketsphynx
+
+      //This will be changed as it is the current file copying method.
       stream.write({rx: data.length / meta.size});
     });
     //run file through sonus
     mainController.sonus();
   });
 });
-//
-//
 
 server.listen(9000);
 
 // START THE SERVER
 // =============================================================================
-
 console.log('Magic happens on port 9000');
