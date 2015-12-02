a4c027251468d6b6b8f243a9ffea625c34ce4933
Seth Champagne <champagneSeth@gmail.com>
2015-12-02 16:18:20 -0600

    Added down sampling through sox

--- sonus/voconomo.js
+++ sonus/voconomo.js
@@ -1,17 +1,32 @@
 var spawn = require('child_process').spawn;
 
 module.exports = function (wavFile, callBack) {
-    var child = spawn(__dirname+ '/sonus.o', [wavFile]);
+    var downSampled = __dirname + '/sonus.wav';
 
-    console.log('Recognizing file : ' + wavFile);
-    child.stdout.on('data', function (data) {
-        var result = /\|([\w\s]+)\|/g.exec(data.toString());
+    ps = spawn('sox', [
+        wavFile
+    ,   '-r', '16k'
+    ,   downSampled
+    ]);
 
-        console.log(result[0]);
-        callBack(result[1]);
+    ps.on('close', function (code) {
+        console.log('\nFinished sox down sample with process ' + code);
+        recognize();
     });
 
-    child.on('close', function (code) { 
-        console.log('\nFinished with process ' + code);
-    });
+    function recognize() {
+        var child = spawn(__dirname + '/sonus.o', [downSampled]);
+
+        console.log('Recognizing file : ' + wavFile);
+        child.stdout.on('data', function (data) {
+            var result = /\|([\w\s]+)\|/g.exec(data.toString());
+
+            console.log(result[0]);
+            callBack(result[1]);
+        });
+
+        child.on('close', function (code) { 
+            console.log('\nFinished sonus.o with process ' + code);
+        });
+    }
 }
